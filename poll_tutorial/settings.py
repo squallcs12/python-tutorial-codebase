1800cc4bd6d1ba8cfe999ed6de0bfdd5edcfb97e
Bang Dao <bang.dao@eastagile.com>
2014-03-21 17:39:24 +0700

    Prepare for integration test

--- poll_tutorial/settings.py
+++ poll_tutorial/settings.py
@@ -120,13 +120,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'polls',
     'south',
     'poll_tutorial',
+    'lettuce.django',
 )
 
 # A sample logging configuration. The only tangible logging
