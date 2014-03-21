c61cc854fec77c40fb01e687a6cbe037d01b4f75
Bang Dao <bang.dao@eastagile.com>
2014-03-21 11:07:47 +0700

    test create choice and question model

--- polls/tests/models/test_choice.py
+++ polls/tests/models/test_choice.py
@@ -1,5 +1,17 @@
 from django.test import TestCase
+import sure
 
 class TestChoice(TestCase):
     def test_choice_model_exists(self):
         from polls.models import Choice
+
+    def test_create_choice(self):
+        from polls.models import Question, Choice
+        question = Question.objects.create(text="Test create choice")
+        choice = Choice.objects.create(question=question, text="test choice text")
+
+        choice2 = Choice(text="test choice 2")
+        question.choices.add(choice2)
+        question.save()
+
+        question.choices.count().should.equal(2)
--- polls/tests/models/test_question.py
+++ polls/tests/models/test_question.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
+import sure
 
 class TestQuestion(TestCase):
     def test_question_model_exists(self):
         from polls.models import Question
+
+    def test_create_question(self):
+        from polls.models import Question
+        question = Question.objects.create(text="Test poll question")
+        question.should_not.be.none
